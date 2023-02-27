d65b44c7024a370825132551c774bc02e72ff877
TDPhantom <abattlessmith@brooklynsteamcenter.org>
2023-02-27 10:52:09 -0500

    New

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html.jinja", 
+    my_variable="Knitter",
+    my_list=["apples", "bananas", "oranges"])
+
+@app.route('/ping')
+def ping():
+    return "<p>pong</p>"
+
+@app.route('/hello/<name>')
+def hello(name):
+    return f"<p>Hello, {name}!</p>"
